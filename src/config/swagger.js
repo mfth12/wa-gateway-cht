a1dd23c7323e729cacb97ce7f2b240ae18423caf
Miftahul Haq <ciftah12@gmail.com>
2026-03-01 01:17:55 +0700

    Update swagger.js

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -4,10 +4,9 @@ const options = {
     definition: {
         openapi: '3.0.0',
         info: {
-            title: 'Chatery WhatsApp API',
-            version: '1.0.0',
-            description: `
-A powerful WhatsApp API backend built with Express.js and Baileys library.
+            title: 'Whatsapp Gateway Cht API',
+            version: '1.4.0',
+            description: `A powerful WhatsApp API backend built with Express.js and Baileys library.
 
 ## Quick Links
 - [🎛️ Dashboard](/dashboard) - Admin Dashboard with API Tester
